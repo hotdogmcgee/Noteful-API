422103af9c8513b65cff4347f8d07c26e4145268
Kevin Robinson <kevinrobinsondeveloper@gmail.com>
2019-08-20 09:37:52 -0400

    add correct db url to config file

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://dunder-mifflin@localhost/noteful',
+    DB_URL: process.env.DB_URL || 'postgres://nxnbaykslscaje:4e96004fa4148da431103effa5aa496a8dc22b8fe90c2fb3214fd4593ef65b3c@ec2-23-21-148-223.compute-1.amazonaws.com:5432/db5dfe128dr6iu',
   }
